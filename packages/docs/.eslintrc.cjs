d2809f11b0c81ce3654e15c45c40076db1715dbe
Italo Sales <italo.x.sales@hotmail.com>
2024-01-07 01:49:13 -0300

    fix: Text component size variant

--- packages/docs/.eslintrc.cjs
+++ packages/docs/.eslintrc.cjs
@@ -1,17 +1,3 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "@ixs-ignite-ui/eslint-config",
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  extends: ['@ixs-ignite-ui/eslint-config'],
 }
--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -17,12 +17,12 @@ const config: StorybookConfig = {
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
+
     options: {},
   },
   docs: {
